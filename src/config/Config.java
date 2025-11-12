bfb43840b072a923c6b7bbc3c74d547bbc7a7074
Misael Lemos <misael.lemos@alu.ufc.br>
2025-11-12 02:44:22 -0300

    feat: Adiciondo novo dashboard e estilo.css

--- src/config/Config.java
+++ src/config/Config.java
@@ -2,11 +2,11 @@ package config;
 
 public final class Config {
     public static final String BD_DRIVER = "org.postgresql.Driver";
-    public static final String BD_URL = "jdbc:postgresql://localhost:5432/postgres";
-    public static final String BD_USUARIO = "postgres";
-    public static final String BD_SENHA = "l1e1v1i1";
+    public static final String BD_URL = "jdbc:postgresql://localhost:5432/ecommerce";
+    public static final String BD_USUARIO = "root";
+    public static final String BD_SENHA = "root";
 
-    private Config(){
+    private Config() {
 
     }
 }
